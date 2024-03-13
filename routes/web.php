f2e43d1339e421c895e166473d22ddc6fcd8f2bd
Fakrul <fakrullah@thebooking360.com>
2024-03-14 06:43:58 +0800

    chore: remove default route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::resource('/api/users',UserController::class)->only(['index','store','show','update','destroy']);
